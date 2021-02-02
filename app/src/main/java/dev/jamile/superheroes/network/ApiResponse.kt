455daa351c3c06e3c66ad7a371e0088a81a8bab8
Jamile <jamilelima@protonmail.com>
2021-02-02 20:51:42 -0300

    feat: add models to handle with network

--- app/src/main/java/dev/jamile/superheroes/network/ApiResponse.kt
+++
@@ -1,7 +0,0 @@
-package dev.jamile.superheroes.network
-
-sealed class ApiResponse<out T> {
-    class Loading(val isLoading: Boolean) : ApiResponse<Boolean>()
-    class Success<out T>(val data: T) : ApiResponse<T>()
-    class Failure(val throwable: Throwable) : ApiResponse<Nothing>()
-}
